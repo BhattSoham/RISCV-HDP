35759d9fdaaefdd4a375d8022eeb297c73ba9fbb
Soham Bhattacharya <98672977+BhattSoham@users.noreply.github.com>
2024-03-08 23:45:43 -0500

    Update alu.c

--- week1/Task2/ALU/alu.c
+++ week1/Task2/ALU/alu.c
@@ -1,49 +1,49 @@
 #include<stdio.h>
 int main() {
 
-int a,b,result ;
-char op;
-printf("Enter two numbers:");
+int a,b,result ; // introducting a,b, result as integers
+char op;  // Operation
+printf("Enter two numbers:"); //printing and scanning the numbers
 scanf("%d %d", &a, &b);
 
-printf("Enter the operation from ADD, SUB, MUL, DIV, OR, AND, XOR : ");
+printf("Enter the operation from ADD, SUB, MUL, DIV, OR, AND, XOR : "); //printing what operation is to be done
 scanf(" %c", &op);
 
 switch(op) {
- case '+':
-   result = a + b;
+ case '+': // Addition
+   result = a + b; 
   break;
  
- case '-':
+ case '-': // Subtraction
     result = a - b;
   break;
 
- case '*':
+ case '*': // Multiplication
     result = a * b;
   break;
 
- case '/':
+ case '/': // Division
     result = a / b;
   break;
 
- case '|':
+ case '|': // OR
    result = a | b;
   break;
 
- case '&':
+ case '&': // AND
    result = a & b;
   break;
 
- case '^':
+ case '^': // XOR
    result = a ^ b;
   break;
 
- default: 
+ default: // If others, wrong operation will be generated
    printf("Wrong operation");
    return 1;
 }
 
-printf("Result is: %d", result);
+printf("Result is: %d", result); // Printing the result
 
 return 0;
 
